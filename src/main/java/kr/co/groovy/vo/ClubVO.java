84f9fd86a1835fc20d9d490f60840ce452bba112
Yun-HaNeul <qnrty12@gmail.com>
2023-09-12 12:54:24 +0900

    clubVO 추가

---
+++ src/main/java/kr/co/groovy/vo/ClubVO.java
@@ -0,0 +1,15 @@
+package kr.co.groovy.vo;
+
+import java.sql.Date;
+
+public class ClubVO {
+    private int clbNo;
+    private String clbNm;
+    private String clbDc;
+    private Date clbDate;
+    private int clbPsncpa;
+    private String clbChirmnEmplId;
+    private String clbKind;
+    private String clbConfmAt;
+
+}
