d542610e82e93da9adf8c6700ce5ca8e222dc0b3
kangseoju <kangseoju@naver.com>
2023-09-16 09:30:19 +0900

    윤하늘 받아라~~

---
+++ src/main/java/kr/co/groovy/vo/TariffVO.java
@@ -0,0 +1,19 @@
+package kr.co.groovy.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class TariffVO {
+    private String taratStdrCode;
+    private String taratStdrYear;
+    private String taratStdrNm;
+    private double taratStdrValue;
+    private String taratStdrApplyCode;
+    private String taratStdrUseAt;
+
+
+}
