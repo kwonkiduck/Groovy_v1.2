480b856e13fe95be5af6d5aadffd2c09211c35f0
Yun-HaNeul <qnrty12@gmail.com>
2023-09-16 09:44:51 +0900

    꺄아아악

--- src/main/java/kr/co/groovy/vo/RecommendVO.java
+++ src/main/java/kr/co/groovy/vo/RecommendVO.java
@@ -7,7 +7,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-public class RecomendVO {
+public class RecommendVO {
     private String sntncEtprCode;
     private String recomendEmplId;
     private int recomendSn;
