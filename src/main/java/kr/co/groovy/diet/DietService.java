dbd9e0819d7011134fec573f397e21fa37d04aa7
suin0704 <fropop74@gmail.com>
2023-09-18 15:09:10 +0900

    식단 업로드 아주 조금 수정 완

--- src/main/java/kr/co/groovy/diet/DietService.java
+++ src/main/java/kr/co/groovy/diet/DietService.java
@@ -1,8 +1,5 @@
 package kr.co.groovy.diet;
 
-import java.util.Date;
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
 import kr.co.groovy.vo.DietVO;
@@ -20,14 +17,6 @@ public class DietService {
 	
 	public int insertDiet(DietVO dietVO) {
 		return dietMapper.insertDiet(dietVO);
-	}
-	
-	public List<DietVO> getAllDiet() {
-		return dietMapper.getAllDiet();
-	}
-	
-	public Date getOneDiet(Date dietDate) {
-		return dietMapper.getOneDiet(dietDate);
 	}
 
 }
